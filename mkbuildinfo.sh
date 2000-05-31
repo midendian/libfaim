9193124799d56ddd6b72f1c5f3cda244dd9a21b0
mid <mid>
2000-05-31 01:21:44 +0000

    - Wed May 31 01:17:24 UTC 2000    - Added constants for message sizes (MAXMSGLEN, MAXCHATMSGLEN)      - Note that the new max message size is defined as 7988bytes,          drastically larger than WinAIM lets you put out.  This is a          libfaim-exclusive feature :)    - Reduced base buffer size on aim_send_im()

--- mkbuildinfo.sh
+++ mkbuildinfo.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 rm -rf aim_meta.o aim_buildcode.h
-echo -n "#define AIM_BUILDDATE " > aim_buildcode.h
-echo `date +%Y%m%e` >> aim_buildcode.h
-echo -n "#define AIM_BUILDTIME " >> aim_buildcode.h
-echo `date +%H%M%S` >> aim_buildcode.h
+
+echo -n "#define AIM_BUILDDATE \"" > aim_buildcode.h
+echo -n `date +%Y%m%e` >> aim_buildcode.h
+echo "\"" >> aim_buildcode.h
+
+echo -n "#define AIM_BUILDTIME \"" >> aim_buildcode.h
+echo -n `date +%H%M%S` >> aim_buildcode.h
+echo "\"" >> aim_buildcode.h
